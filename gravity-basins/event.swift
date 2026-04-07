53c3e5091b1e61d38813c77ca4f75adec8069073
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2026-04-07 09:59:27 +0000

    Remove log event

--- gravity-basins/event.swift
+++ gravity-basins/event.swift
@@ -6,7 +6,6 @@ enum source_t {
 }
 
 enum event_t {
-//	case log(_ message : String)
 	case in_motion(_ in_motion : Bool)
 	case resolution(_ source : source_t, _ display_scale : CGFloat, _ resolution : CGSize)
 	case single_tap(_ source : source_t, _ position : CGPoint, _ resolution : CGSize)
